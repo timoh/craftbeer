6f3dcf453d23d90b62db5fdea88a9cd86f5bdd9c
Timo Herttua <timo.herttua@efekta.fi>
2015-10-26 20:13:15 +0200

    availability model takes into account funky date formats

--- app/models/alco_avail.rb
+++ app/models/alco_avail.rb
@@ -8,7 +8,6 @@ class AlcoAvail
   belongs_to :alco_drink
 
   validates :amount, presence: true
-  validates :last_updated, presence: true
 
   def AlcoAvail.get_for_prod(prod_id='781706', city='helsinki')
     require 'net/http'
@@ -41,11 +40,23 @@ class AlcoAvail
       av.alco_location = loc_obj
 
       av.amount = res_loc["Amount"]
-      # format of data is "LastUpdated" => "17.10."
+      final_date = ""
+      # format of data is "LastUpdated" => "17.10.", but might also be "eilen" or "2015-10-19"!
       # target is: strptime("17.10.2015", "%d.%m.%Y")
-      final_date = res_loc["LastUpdated"]+(Date.today.year.to_s)
-      puts final_date
-      av.last_updated = Date.strptime(final_date, "%d.%m.%Y")
+      if res_loc["LastUpdated"] == "eilen"
+        final_date = Date.today-1
+      elsif res_loc["LastUpdated"].include? "-"
+        final_date = res_loc["LastUpdated"]
+        puts final_date
+        av.last_updated = Date.strptime(res_loc["LastUpdated"], "%Y-%m-%d")
+      else
+        final_date = res_loc["LastUpdated"]+(Date.today.year.to_s)
+        puts final_date
+        av.last_updated = Date.strptime(final_date, "%d.%m.%Y")
+      end
+
+
+
 
       puts av
 
