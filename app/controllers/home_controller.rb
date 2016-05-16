7f5bbb3bf2f1038813118c7dcbfa611539152f05
Timo Herttua <timo.herttua@efekta.fi>
2016-05-16 18:42:37 +0300

    home/index.json now has also review and availability data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     @drinks = AlcoDrink.where(:best_rev_candidate_score.gte => 0.1).order_by(best_rev_candidate_score: "desc")
-   
+
     respond_to do |format|
         format.html
-        format.json { render json: @drinks }
+        format.json { render json: @drinks.to_json(:include => [:alco_avails, :review]) }
     end
   end
 end
